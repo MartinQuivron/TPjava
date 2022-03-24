967a8eea7d522ad4417b5eddfd3700e87acb28fa
Eremith <loic.gdl2001@gmail.com>
2022-03-24 11:51:15 +0100

    ingredient

--- src/App.java
+++ src/App.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        
+
         System.out.println("Quel écran souhaitez-vous afficher ?");
         System.out.println("1- Écran prise de commande");
         System.out.println("2- Écran cuisine");
         System.out.println("3- Écran bar");
         System.out.println("4- Écran Monitoring");
-        
+
         Scanner scanner = new Scanner(System.in);
-        
+
         int choixEcran = scanner.nextInt();
-        
+
         Plat burger = new Plat("pain", "salade", "steack");
         burger.afficherIngredients();
 
@@ -36,29 +36,39 @@ class Personne{
 }
 
 class Lieu{
-    
+
 }
 
 class Carte{
-    
+
 }
 
 class Commande{
-    
+
 }
 
 class Plat {
     String ingredients[];
-    
+
     Plat(String...ingredients){
         if(ingredients != null) {
             this.ingredients = Arrays.copyOf(ingredients, ingredients.length);
         }
     }
-    
+
     public void afficherIngredients() {
         for(final String ingredient : ingredients) {
             System.out.println(ingredient);
         }
     }
+}
+
+class Ingredient {
+
+    String NomIngredient;
+
+    Ingredient(String nom) {
+      this.NomIngredient = nom;
+    }
+
 }
